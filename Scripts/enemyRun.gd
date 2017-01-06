3eacf2d489daf6acb23fc1fea4d83b7d84b5966c
cypher31 <kelbystyler@gmail.com>
2017-01-05 18:31:00 -0800

    Enemy update
    Enemy destroys themself if player enters them then defeats them.

--- Scripts/enemyRun.gd
+++ Scripts/enemyRun.gd
@@ -5,12 +5,14 @@ func _ready():
 	pass
 
 func _fixed_process(delta):
+	if(get_node("/root/global").currentRunEnemy == true):
+		self.queue_free()
 	pass
 
 func _on_playerCheck_body_enter( body ):
 	if(body.is_in_group("playerRun")):
 		get_node("/root/global").playerStartPosition = self.get_global_pos()
-#		get_node("/root/global").currentRunEnemy = self
+		get_node("/root/global").currentRunEnemy = true
 		get_node("/root/global").setScene("res://Scenes/Dungeon_Fight.tscn")
 	pass
 
